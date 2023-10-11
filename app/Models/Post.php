bc1c3e30ad409031da4b2cf56c563516fbfb873c
mhayashi91 <awesome_box@icloud.com>
2023-10-11 23:32:10 +0800

    post.php多分OK

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+    public function tags()
+    {
+        return $this->belongsToMany('App\Models\Tag');
+    }
+    public function post_tags()
+    {
+        return $this->belongsToMany('App\Models\Post_tag');
+    }
+    public function bookmarks()
+    {
+        return $this->hasMany('App\Models\Bookmark');
+    }
+    public function likedBy($user)
+    {
+        return Bookmark::where('user_id', $user->id)->where('post_id',$this->id);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment');
+    }
 }
