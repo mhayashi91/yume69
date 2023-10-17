7a61f848cd65cb79229cfcafd04aa88aedfc71be
mhayashi91 <awesome_box@icloud.com>
2023-10-17 21:25:33 +0800

    CommentController記述

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
     //
+    public function create($post_id)
+    {
+        $post = Post::find($post_id);
+        return view('comments.create', compact('post'));
+    }
+
+    public function store(Request $request)
+    {
+        $post = Post::find($request->post_id);
+        $comment = new Comment;
+        $comment -> body = $request -> body;
+        $comment -> user_id = Auth::id();
+        $comment -> post_id = $request -> post_id;
+        $comment -> save();
+    
+        return redirect()->route('comments.show',$post->id);
+    }
 }
