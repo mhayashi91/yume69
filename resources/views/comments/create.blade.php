ce9290afc29eef9ad1b7a1914541dea8033fd5ba
mhayashi91 <awesome_box@icloud.com>
2023-10-18 10:57:28 +0800

    css整え中

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -1,16 +1,26 @@
 @extends('layouts.app')
 @section('content')
 
+<link rel="stylesheet" type="text/css" href="{{ asset('css/comment.css') }}">
+
 <div class="big-container">
     
     <div class="middle-container-1">
         <div class="small-container-1">
-            <h2>以下の記事にコメントします</h2>
             <div class="content-container">
+                <h2>以下の記事にコメントします</h2>
                 <div class="card-header">
-                    <h5>タイトル：{{ $post->title }}</h5>
+                        <div class="image-container">
+                            <a href="{{ route('show', [$post->user->id]) }}">
+                                <img src="{{ asset('storage/images/' . $post->user->avatar) }}" alt="Image" width="70px" height="70px">
+                            </a>
+                        </div>
+                        <div class="name-box">
+                            <h3 class="name">{{ $post->user->name }}</h3>
+                        </div>
                 </div>
                 <div class="card-body">
+                    <h5>タイトル：{{ $post->title }}</h5>
                     <p class="card-text">内容：{{ $post->contents }}</p>
                     <p>投稿日時：{{ $post->created_at }}</p>
                 </div>
@@ -24,11 +34,14 @@
                 @csrf
                 <input type="hidden" name="post_id" value="{{ $post->id }}">
                 <div class="form-group">
-                    <label>コメント</label>
+                    <h2>コメント</h2>
                     <textarea class="form-control" 
                     placeholder="内容" rows="5" name="body"></textarea>
                 </div>
-                <button type="submit" class="commentbutton">コメントする</button>
+                <div class="post-buttons">
+                <button type="submit" class="cancel">キャンセル</button>
+                <button type="submit" class="comment-button">コメントする</button>
+                </div>
             </form>
         </div>
     </div>
