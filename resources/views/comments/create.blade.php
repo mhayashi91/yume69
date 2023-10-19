3c72b43c5ee2f8a00938518aba1cab570d24b178
mhayashi91 <awesome_box@icloud.com>
2023-10-19 10:50:32 +0800

    jsはなしにした。

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/comment.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('css/comment.js') }}">
+{{-- <link rel="stylesheet" type="text/css" href="{{ asset('css/comment.js') }}"> --}}
 <div class="big-container">
     
     <div class="middle-container-1">
@@ -31,43 +31,19 @@
 
     <div class="middle-container-2">
         <div class="small-container-2">
-            <form action="{{ route('comments.store') }}" method="post" onsubmit="return validateComment()">
+            <form action="{{ route('comments.store') }}" method="post">
                 @csrf
                 <input type="hidden" name="post_id" value="{{ $post->id }}">
                 <div class="form-group">
                     <h3>コメント</h3>
                     <textarea class="form-control" 
-                    placeholder="内容" rows="5" name="body" oninput="checkCommentInput(this)"></textarea>
+                    placeholder="内容" rows="5" name="body"></textarea>
                 </div>
                 <div class="post-buttons">
                     <button type="button" class="cancel" onclick="window.location='{{ route('posts.index') }}'">キャンセル</button>
-                    <button type="submit" class="comment-button" id="commentSubmitButton" style="display: none;">コメントする</button>
+                    <button type="submit" class="comment-button">コメントする</button>
                 </div>
             </form>
-            <script>
-                function checkCommentInput(textarea) {
-                    var commentSubmitButton = document.getElementById('commentSubmitButton');
-                    if (textarea.value.trim() !== '') {
-                        commentSubmitButton.style.display = 'block';
-                    } else {
-                        commentSubmitButton.style.display = 'none';
-                    }
-                }
-            
-                function validateComment() {
-                    var commentText = document.querySelector('textarea[name="body"]').value;
-                    
-                    if (commentText.trim() === '') {
-                        // コメントが空の場合にアラートメッセージを表示
-                        alert('コメントを入力してください。');
-                        return false; // フォームの送信を中止
-                    }
-                    
-                    // コメントが入力されている場合はフォームを送信
-                    return true;
-                }
-            </script>
-            
         </div>
     </div>
 
