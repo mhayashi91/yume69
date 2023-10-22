afab2c1ce2a6575c975aadce808ddddafc1d3295
mhayashi91 <awesome_box@icloud.com>
2023-10-22 19:56:35 +0800

    入力dd確認

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 use App\Models\Post;
+use App\Models\Tag;
+use App\Models\Post_tag;
 use Illuminate\Support\Facades\Auth; 
 
 use Illuminate\Http\Request;
@@ -32,10 +34,23 @@ class PostController extends Controller
         $post = new Post;
         $post -> title = $request -> title;
         $post -> contents = $request -> contents;
-        // $post -> hassyu = $request -> hassyu;
+        $post -> hassyu = $request -> hassyu;
         $post -> user_id = Auth::id();
 
         $post -> save();
+
+        //DBトランザクション
+        DB::transaction(function() use($post){
+            $post_id = Post::insertGetId(['contents' => $post['contents'], 'user_id' => \Auth::id()]);
+            dd($post);
+            // $tag_exists = Tag::where('user_id', '=', \Auth::id())->where('name', '=', $post['hassyu'])
+            // ->exists();
+
+            // if( !empty($post['hassyu']) && !$tag_exists ){
+            //     $tag_id = Tag::insertGetId(['user_id' => \Auth::id(), 'name' => $post['hassyu']]);
+            //     Post_tag::insert(['post_tag' => $post_id, 'tag_id' => $tag_id]);
+            // }
+        });
 
         return redirect()->route('posts.index');
     }
