014e95187fc18e7414e99e0c6d1899b8b6a5ca78
mhayashi91 <awesome_box@icloud.com>
2023-10-23 15:18:06 +0800

    pagination.cssに記載

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/pagination.css') }}">
         <link rel="stylesheet" href="{{ asset('css/index.css') }}">
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
         <script src="{{ asset('css/app.js') }}"></script>
