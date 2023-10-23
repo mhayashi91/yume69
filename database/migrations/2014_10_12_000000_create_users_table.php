f463219892fb9768b264454bb5d16d9c96db8818
otst <121769026+otst369@users.noreply.github.com>
2023-10-23 12:52:35 +0800

    登録ページで＊をつけた、編集前の記述残した、名前のリンク化、

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 30);
+            $table->string('name', 15);
             $table->string('email', 30)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 90);
