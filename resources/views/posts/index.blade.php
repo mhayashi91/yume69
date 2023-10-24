91907edfbca2d10fb293621c2d3d470511730778
mhayashi91 <awesome_box@icloud.com>
2023-10-24 09:23:39 +0800

    commit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -58,7 +58,8 @@
                             <p class="contents">{{ $post->contents }}</p>
                         </div>
                         @foreach ($post->tags as $tag)
-                            <a href="{{ route('tags.search', ['tag' => $tag->tag_name]) }}">#{{ $tag->tag_name }}</a>
+                            {{-- <a href="{{ route('tags.search', ['tag' => $tag->tag_name]) }}">#{{ $tag->tag_name }}</a> --}}
+                            <a href="{{ route('tags.search', ['tag' => $tag->tag_name]) }}" class="btn btn-sm btn-primary m-1">#{{ $tag->tag_name }}</a>
                         @endforeach
 
                         
